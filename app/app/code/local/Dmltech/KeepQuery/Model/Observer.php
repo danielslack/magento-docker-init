88ea6b3df5bb8cf0266ee30708b1054eed00de5e
Daniel Caria <daniel@dmltech.com.br>
2023-08-05 11:16:57 -0300

    Salvando Log para validação

--- app/app/code/local/Dmltech/KeepQuery/Model/Observer.php
+++ app/app/code/local/Dmltech/KeepQuery/Model/Observer.php
@@ -4,17 +4,17 @@ class Dmltech_KeepQuery_Model_Observer
 {
     public function salvarQuerystring(Varien_Event_Observer $observer)
     {
-        // $controllerAction = $observer->getControllerAction();
-        // $request = $controllerAction->getRequest();
+        $controllerAction = $observer->getControllerAction();
+        $request = $controllerAction->getRequest();
 
-        // $promo = $request->getParam('promo_code');
+        $promo = $request->getParam('promo_code');
 
-        // if ($promo) {
-        //     Mage::getSingleton('customer/session')->setData('promo_code', $promo);
-        // }
+        if ($promo) {
+            Mage::getSingleton('customer/session')->setData('promo_code', $promo);
+        }
 
-        // // Recupera da sessao e salva um log
-        // $promo = Mage::getSingleton('customer/session')->getData('promo_code');
-        // Mage::log($promo, null, 'dmltech.log', true);
+        // Recupera da sessao e salva um log
+        $promo = Mage::getSingleton('customer/session')->getData('promo_code');
+        Mage::log($promo, null, 'dmltech.log', true);
     }
 }
